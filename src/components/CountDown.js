1fd010b5992401321cc391321ad5fde8e8a617c2
Bonyonte <godfrey.magogo@student.moringaschool.comgit config --global user.name Bonyontegit branch -m maingit branch -M maingit remote add origin git@github.com:Bonyonte/ip_1.gitgit push -u origin maingit config --global user.email godfrey.magogo@student.moringaschool.com>
2022-07-14 12:44:29 +0300

    how to use state

--- src/components/CountDown.js
+++ src/components/CountDown.js
@@ -4,14 +4,32 @@ export class CountDown extends React.Component{
 
     constructor(props){
         super(props)
+        this.state = {count: 100, name: 'Godfrey'}
     }
 
+    componentDidMount(){
+        this.update();
+    }
+
+    update(){
+        
+        setInterval(
+            () => {
+                const currentCount = this.state.count - 1
+                this.setState({count: currentCount})
+            }, 1000
+            //TODO: IDENTIFY WHY COUNT REDUCES BY 2 INSTEAD OF 1
+            //TODO: FIND HOW TO STOP THE COUNT AT 0
+        )
+        
+
+    }
 
     render(){
         return(
             <div>
                 <center>
-                    <h4 className="time-text">100</h4>
+                    <h4 className="time-text">{this.state.count}</h4>
                 </center>
             </div>
         )
